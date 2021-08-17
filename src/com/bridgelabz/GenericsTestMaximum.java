117a01c4c50cea4a5c86cb5adab108bed8efa0aa
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-17 18:50:07 +0530

    Added: Find the maximum between three integers

--- src/com/bridgelabz/GenericsTestMaximum.java
+++ src/com/bridgelabz/GenericsTestMaximum.java
@@ -1,8 +1,22 @@
 package com.bridgelabz;
 
 public class GenericsTestMaximum {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Generics Test Maximum");
+    // determines the largest of three Comparable objects
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+        T max = x; // assume x is initially the largest
+
+        if (y.compareTo(max) > 0)
+            max = y; // y is the largest so far
+
+        if (z.compareTo(max) > 0)
+            max = z; // z is the largest
+
+        return max; // returns the largest object
+    } // end method maximum
+
+    public static void main(String args[]) {
+        System.out.printf("Maximum of %d, %d and %d is %d\n\n", 5, 4, 3, maximum(5, 4, 3));
+
     }
 }
 
